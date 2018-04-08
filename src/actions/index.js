0da490350d6b6042632bfe5823818ae644601ca8
Hayk <hayk.pilos@gmail.com>
2018-04-08 04:07:44 +0400

    Subscribe for events
    Change-Id: I6d6a512f73a6b1aea37dd3f71afda7ff41cb66e5

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,8 @@ import * as api from '../api';
 import * as types from '../constants/types';
 import composeInvoiceUBL from '../api/ubl';
 
+let ws;
+
 export function addInvoice(invoice, uid) {
   const formData = new FormData();
   const payload = composeInvoiceUBL(invoice) ;
@@ -31,3 +33,72 @@ export function getInvoice(url) {
     })
   };
 };
+
+export function scheduleInvoice(firestore_id, payload) {
+  return (dispatch) => {
+    const name = 'document-add';
+    api.scheduleInvoice(name, payload, (data) => {
+      const schedule_id = data.id;
+      // Store the mapping of public id in mongo to id in firestore
+      // for future reference
+      localStorage.setItem(schedule_id, firestore_id);
+
+      dispatch({
+        type: types.INVOICE_SCHEDULED,
+        data,
+      });
+    })
+  };
+};
+
+export function subscribeToTopics(topics) {
+  return (dispatch) => {
+    api.subscribe(topics, (data) => {
+      subscribe(data.url);
+      dispatch({
+        type: types.TOPICS_SUBSCRIBED,
+        data,
+      });
+    })
+  };
+};
+
+export function subscribe(url) {
+  const ws_url = `ws://localhost:8888${url}`;
+  ws = new WebSocket(ws_url);
+
+  ws.onopen = function()
+  {
+    console.log('< opened connection');
+    ws.send(JSON.stringify({ name: 'confirm', payload: {} }));
+  };
+
+  ws.onmessage = function (m)
+  {
+    console.log(`> received`, m);
+    const data = JSON.parse(m.data);
+    const { topic, payload } = data;
+    if (topic === 'xadf.compute.documents') {
+      const id = payload;
+      console.log(id);
+    }
+
+    if (topic === 'xadf.compute.effective') {
+      const id = payload.split(':')[0];
+      console.log(id);
+    }
+  };
+
+  ws.onclose = function()
+  {
+    // websocket is closed.
+    console.log("Connection is closed...");
+  };
+};
+
+export function disconnect() {
+  ws.close();
+
+  return ()=>{}
+}
+
--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,7 @@
 // TODO: Move this to config
 const DOCUMENT_SERVICE = 'https://lichen-ui.appspot.com/v1';
+const SCHEDULE_SERVICE = 'http://localhost:9000';
+const EVENTS_SERVICE = 'http://localhost:4200';
 
 export function addInvoice (invoice, uid, callback) {
   fetch(`${DOCUMENT_SERVICE}/documents`, {
@@ -28,4 +30,37 @@ export function getInvoice(path, callback) {
   }).catch((err) => {
     alert(`Error: ${err}`);
   });
-}
+};
+
+export function scheduleInvoice(name, payload, callback) {
+  fetch(`${SCHEDULE_SERVICE}/actions`, {
+    body: JSON.stringify({
+      name,
+      payload,
+    }),
+    headers: {'Content-Type': 'application/json'},
+    method: 'POST',
+  })
+  .then(response => response.json())
+  .then((data) => {
+    callback(data);
+  }).catch((err) => {
+    alert(`Error: ${err}`);
+  });
+};
+
+export function subscribe(topics, callback) {
+  fetch(`${EVENTS_SERVICE}/subscriptions`, {
+    body: JSON.stringify({
+      topics: ['xadf.compute.documents', 'xadf.compute.effective']
+    }),
+    headers: {'Content-Type': 'application/json'},
+    method: 'POST',
+  })
+  .then(response => response.json())
+  .then((data) => {
+    callback(data);
+  }).catch((err) => {
+    alert(`Error: ${err}`);
+  });
+};
